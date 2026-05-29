41f21e203f4762deca18667127d5233ead89150e
a-c-d-c <info@ansgar-nitsche.de>
2026-05-29 17:34:02 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,14 @@
 self.addEventListener('install', () => self.skipWaiting());
-self.addEventListener('activate', e => e.waitUntil(self.clients.claim()));
-self.addEventListener('fetch', e => e.respondWith(
-  fetch(e.request).catch(() => caches.match(e.request))
-));
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+self.addEventListener('fetch', e => {
+  const req = e.request.url.startsWith(self.location.origin)
+    ? new Request(e.request, { cache: 'no-cache' })
+    : e.request;
+  e.respondWith(fetch(req).catch(() => caches.match(e.request)));
+});
